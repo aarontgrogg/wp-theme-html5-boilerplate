8a00e6bfda5023994419496eccb811eef67643d0
Max Fenton <max@maxfenton.com>
2015-07-13 18:57:26 -0400

    Check that comments haven't already been removed
    Double-check that comments haven't already been removed by another plugin.

--- functions.php
+++ functions.php
@@ -465,7 +465,9 @@ if ( ! function_exists( 'boilerplate_remove_recent_comments_style' ) ) :
 	 */
 	function boilerplate_remove_recent_comments_style() {
 		global $wp_widget_factory;
-		remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+		if ( !empty( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
+			remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+		}
 	}
 endif;
 add_action( 'widgets_init', 'boilerplate_remove_recent_comments_style' );
